7b3ce30b9ae9f6cb7856e50845d9d5c4f254802f
Stephen <stephenanosike18@gmail.com>
2025-06-20 16:15:47 +0100

    third section of the About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import CoreValue from '../components/CoreValue'
 
 const About = () => {
   return (
     <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum magni non, unde harum, nulla, ipsum animi modi maiores tempora nisi error odit quia officia laboriosam cupiditate asperiores possimus ad ratione!
+     <CoreValue/>
     </div>
   )
 }
